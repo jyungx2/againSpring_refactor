4d5d7c5cc468d08a634f6925a5be381d63b3575c
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-26 11:00:16 +0900

    🐛 fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
     </Router>
