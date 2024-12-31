862dbd3bc48eefb324be42feaee304f9e52af5de
jiyoungnim <cloundyon31@gmail.com>
2024-12-31 22:52:26 +0900

    👏

---
+++ src/pages/user/Myreview.jsx
@@ -0,0 +1,17 @@
+function Myreview() {
+  return (
+    <>
+      <div className="sidebar">
+        <a>주문조회</a>
+        <a>1:1 문의</a>
+        <a>위시리스트</a>
+        <a>쿠폰</a>
+        <a>포인트</a>
+        <a>정보 수정</a>
+        <a>회원탈퇴</a>
+      </div>
+    </>
+  );
+}
+
+export default Myreview;
