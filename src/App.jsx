87018ab1267d089953426cc03cea60323537c51c
ohDASEUL <comeseul312@gmail.com>
2025-01-02 17:19:45 +0900

    🚚 [Q&A] 브라우저 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/routes';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import ListPage from './pages/qna/ListPage';
+import NewPostPage from './pages/qna/NewPostPage';
+import PostDetailPage from './pages/qna/PostDetailPage';
+import EditPostPage from './pages/qna/EditPostPage';
 
 const App = () => {
   return (
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <BrowserRouter>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<Navigate to='/qna' replace />} />
+
+            <Route path='/qna'>
+              <Route index element={<ListPage />} />
+              <Route path='new' element={<NewPostPage />} />
+              <Route path='detail' element={<PostDetailPage />} />
+              <Route path='edit' element={<EditPostPage />} />
+            </Route>
+
+            <Route path='*' element={<Navigate to='/qna' replace />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,5 @@
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
-
 const Layout = () => {
-  return (
-    <div className='min-h-screen flex flex-col'>
-      <div className='fixed top-0 left-0 right-0 z-50'>
-        <Header />
-      </div>
-      <main className='flex-grow mt-[132px]'>
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <></>;
 };
 
 export default Layout;
