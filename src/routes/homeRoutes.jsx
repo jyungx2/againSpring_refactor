9a00f245de81ecb630c170367bae7448d0c0afe3
ohDASEUL <comeseul312@gmail.com>
2025-01-04 00:52:26 +0900

    🚚 [공지사항] routes.jsx 경로 수정

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,8 +1,16 @@
-import Login from '@pages/user/Login';
+import EditPostPage from '@pages/notice/EditPostPage';
+import ListPage from '@pages/notice/ListPage';
+import NewPostPage from '@pages/notice/NewPostPage';
+import PostDetailPage from '@pages/notice/PostDetailPage';
+// import Login from '@pages/user/Login';
 
 const homeRoutes = [
   // { index: true, element: <mainLayout /> },
-  { path: 'login', element: <Login /> },
+  // { path: 'login', element: <Login /> },
+  { path: 'notice', element: <ListPage /> },
+  { path: 'notice/new', element: <NewPostPage /> },
+  { path: 'notice/edit', element: <EditPostPage /> },
+  { path: 'notice/detail', element: <PostDetailPage /> },
 ];
 
 export default homeRoutes;
