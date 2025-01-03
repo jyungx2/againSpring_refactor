eb1e6003acb91846071fafd7f6ada20474bd1226
jiyoungnim <cloundyon31@gmail.com>
2025-01-03 16:46:40 +0900

    📝 상품리뷰 페이지 라우터 경로 추가

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,8 +1,10 @@
 import Login from "@pages/user/Login";
+import Myreview from "@pages/user/Myreview";
 
 const homeRoutes = [
   // { index: true, element: <mainLayout /> },
   { path: "login", element: <Login /> },
+  { path: "review", element: <Myreview /> },
 ];
 
 export default homeRoutes;
