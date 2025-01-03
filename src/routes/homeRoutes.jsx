0cf4c9e3040be0486eeb63848e15cd686a215b93
ohDASEUL <comeseul312@gmail.com>
2025-01-04 01:20:35 +0900

    🚚 [Q&A] 라우터 경로 수정

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,8 +1,17 @@
-import Login from '@pages/user/Login';
+// import Login from '@pages/user/Login';
+
+import EditPostPage from '@pages/QnA/EditPostPage';
+import ListPage from '@pages/QnA/ListPage';
+import NewPostPage from '@pages/QnA/NewPostPage';
+import PostDetailPage from '@pages/QnA/PostDetailPage';
 
 const homeRoutes = [
   // { index: true, element: <mainLayout /> },
-  { path: 'login', element: <Login /> },
+  // { path: 'login', element: <Login /> },
+  { path: 'qna', element: <ListPage /> },
+  { path: 'qna/new', element: <NewPostPage /> },
+  { path: 'qna/edit', element: <EditPostPage /> },
+  { path: 'qna/detail', element: <PostDetailPage /> },
 ];
 
 export default homeRoutes;
