ce6f97f0f8c136f0c2a5fe05c612764a892f9d2a
ohDASEUL <comeseul312@gmail.com>
2025-01-03 10:39:11 +0900

    🚚 [공지사항] 라우터 구조 확장

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,11 +1,46 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
+import { Navigate } from 'react-router-dom';
+import Home from '../pages/Home';
+import ListPage from '../pages/notice/ListPage';
+import NewPostPage from '../pages/notice/NewPostPage';
+import PostDetailPage from '../pages/notice/PostDetailPage';
+import EditPostPage from '../pages/notice/EditPostPage';
 
-const router = createBrowserRouter([
+const routes = [
   {
-    path: "/",
+    path: '/',
     element: <Home />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to='/notice' replace />,
+      },
+      {
+        path: 'notice',
+        children: [
+          {
+            index: true,
+            element: <ListPage />,
+          },
+          {
+            path: 'new',
+            element: <NewPostPage />,
+          },
+          {
+            path: 'detail',
+            element: <PostDetailPage />,
+          },
+          {
+            path: 'edit',
+            element: <EditPostPage />,
+          },
+        ],
+      },
+      {
+        path: '*',
+        element: <Navigate to='/notice' replace />,
+      },
+    ],
   },
-]);
+];
 
-export default router;
+export default routes;
