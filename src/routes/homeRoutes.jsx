907cab151db2a252ab1599da426a347c5adfb1b0
ohDASEUL <comeseul312@gmail.com>
2025-01-03 11:18:23 +0900

    🚚 [Q&A] 라우터 구조 확장

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,11 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from '../pages/Home';
+import ListPage from '../pages/qna/ListPage';
+import NewPostPage from '../pages/qna/NewPostPage';
+import PostDetailPage from '../pages/qna/PostDetailPage';
+import EditPostPage from '../pages/qna/EditPostPage';
 
-const router = createBrowserRouter([
+const homeRoutes = [
   {
-    path: "/",
+    path: '/',
     element: <Home />,
+    children: [
+      {
+        path: 'qna',
+        children: [
+          {
+            index: true,
+            element: <ListPage />,
+          },
+          {
+            path: 'new',
+            element: <NewPostPage />,
+          },
+          {
+            path: 'detail',
+            element: <PostDetailPage />,
+          },
+          {
+            path: 'edit',
+            element: <EditPostPage />,
+          },
+        ],
+      },
+    ],
   },
-]);
+];
 
-export default router;
+export default homeRoutes;
