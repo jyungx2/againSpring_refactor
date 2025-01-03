028d6e398b71ee6034e27372520379ad0ebaaef6
ohDASEUL <comeseul312@gmail.com>
2025-01-03 10:40:04 +0900

    🚚 [공지사항] 레이아웃에 Outlet 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,15 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       {/* Header */}
       <Header />
 
       {/* Main Content */}
-      <main className="flex-grow px-6">
-        <h1 className="text-2xl font-bold">main contents</h1>
-      </main>
+      <Outlet />
 
       {/* Footer */}
       <Footer />
