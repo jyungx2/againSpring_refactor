c1537559ca5d953eda92d2bc4c73e7f1965c96c7
jiyoungnim <cloundyon31@gmail.com>
2025-01-03 23:30:13 +0900

    💄 순수CSS -> 테일윈드CSS 로 변경완료 (문의하러가기 버튼 보더가 안그려지는 문제 발생)

--- src/pages/user/Myquery.jsx
+++ src/pages/user/Myquery.jsx
@@ -1,11 +1,11 @@
-import "./main.css";
+import "./jy-global.css";
 
 function Myquery() {
   return (
     <>
-      <div className="container">
-        <div className="wrapper">
-          <div className="sidebar">
+      <div className="max-w-[1580px] py-28 px-6 pb-0">
+        <div className="flex gap-[20px]">
+          <div className="flex flex-col gap-[24px] pt-[24px] min-w-[180px]">
             <a>주문조회</a>
             <a>1:1 문의</a>
             <a>위시리스트</a>
@@ -15,65 +15,111 @@ function Myquery() {
             <a>회원탈퇴</a>
           </div>
 
-          <div className="main">
-            <div className="l_section top">
-              <div className="img-section">
+          <div className="flex-grow min-w-0 basis-0 flex flex-col gap-[40px]">
+            <div className="flex border-t-2 border-black p-[30px] gap-[18px] items-start ">
+              <div className="flex-shrink-0">
                 <img src="src/pages/user/icons/query.svg" />
               </div>
 
-              <div className="first-depth">
-                <h1>문의 내역 확인</h1>
-                <div className="btn-section">
-                  <button className="button">문의하러 가기</button>
+              <div className="flex flex-col gap-[40px] flex-grow">
+                <h1 className="font-gowunBold text-[26px]">문의 내역 확인</h1>
+                <div className="ml-auto">
+                  <button className="font-gowunBold inline-block h-[42px] text-primary-70 text-[16px] px-[24px] border border-primary-30 rounded-[2px] box-border cursor-pointer">
+                    문의하러 가기
+                  </button>
                 </div>
               </div>
             </div>
 
-            <div className="l_section middle">
-              <h2>문의내역</h2>
+            <div className="flex flex-col gap-[10px] justify-center border-t border-grey-30 p-[30px]">
+              <h2 className="font-gowunBold text-[22px]">문의내역</h2>
 
-              <table>
-                <thead>
+              <table className="w-full border-collapse my-[20px]">
+                <thead className="bg-grey-5 font-gowunBold">
                   <tr>
-                    <th>번호</th>
-                    <th>제목</th>
-                    <th>작성일</th>
-                    <th>작성자</th>
-                    <th>조회수</th>
+                    <th className="border border-grey-30 text-center p-[8px]">
+                      번호
+                    </th>
+                    <th className="border border-grey-30 text-center p-[8px]">
+                      제목
+                    </th>
+                    <th className="border border-grey-30 text-center p-[8px]">
+                      작성일
+                    </th>
+                    <th className="border border-grey-30 text-center p-[8px]">
+                      작성자
+                    </th>
+                    <th className="border border-grey-30 text-center p-[8px]">
+                      조회수
+                    </th>
                   </tr>
                 </thead>
 
                 <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>배송은 보통 며칠 걸리나요?</td>
-                    <td>2024-12-02</td>
-                    <td>국연수</td>
-                    <td>2</td>
+                  <tr className="hover:bg-primary-5 hover:cursor-pointer">
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      1
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      배송은 보통 며칠 걸리나요?
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      2024-12-02
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      국연수
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      2
+                    </td>
                   </tr>
 
-                  <tr>
-                    <td>2</td>
-                    <td>교환이나 환불 신청은 며칠 이내로 해야 하나요?</td>
-                    <td>2024-12-20</td>
-                    <td>국연수</td>
-                    <td>8</td>
+                  <tr className="hover:bg-primary-5 hover:cursor-pointer">
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      2
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      교환이나 환불 신청은 며칠 이내로 해야 하나요?
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      2024-12-20
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      국연수
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      8
+                    </td>
                   </tr>
 
-                  <tr>
-                    <td>2</td>
-                    <td>유통기한이 언제까지 인가요?</td>
-                    <td>2024-12-20</td>
-                    <td>국연수</td>
-                    <td>8</td>
+                  <tr className="hover:bg-primary-5 hover:cursor-pointer">
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      3
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      유통기한이 언제까지 인가요?
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      2024-12-20
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      국연수
+                    </td>
+                    <td className="border border-grey-30 text-center p-[8px]">
+                      8
+                    </td>
                   </tr>
                 </tbody>
               </table>
             </div>
 
-            <div className="l_section bottom">
-              <button className="btn btn-cancel">취소</button>
-              <button className="btn btn-register">등록</button>
+            <div className="flex gap-[12px] justify-center border-t border-grey-30 p-[30px]">
+              <button className="font-gowunBold inline-block text-[16px] text-white h-[48px] leading-[48px] px-[64px] box-border cursor-pointer rounded-[12px] bg-grey-40">
+                취소
+              </button>
+              <button className="font-gowunBold inline-block text-[16px] text-white h-[48px] leading-[48px] px-[64px] box-border cursor-pointer rounded-[12px] bg-primary-40">
+                등록
+              </button>
             </div>
           </div>
         </div>
--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,8 +1,10 @@
 import Login from "@pages/user/Login";
+import Myquery from "@pages/user/Myquery";
 
 const homeRoutes = [
   // { index: true, element: <mainLayout /> },
   { path: "login", element: <Login /> },
+  { path: "query", element: <Myquery /> },
 ];
 
 export default homeRoutes;
