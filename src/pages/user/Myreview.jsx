f535c06ad81152cb33559e31e4edcfa7b275ee38
jiyoungnim <cloundyon31@gmail.com>
2025-01-03 17:58:29 +0900

    💄 순수CSS -> 테일윈드CSS 로 변경완료 (사진 첨부하기 버튼 보더가 안 그려지는 문제 발생)

--- src/pages/user/Myreview.jsx
+++ src/pages/user/Myreview.jsx
@@ -1,12 +1,11 @@
-import "./main.css";
 import "./jy-global.css";
 
 function Myreview() {
   return (
     <>
-      <div className="max-w-[1580px] mx-auto p-[24px] pb-0">
-        <div className="flex">
-          <div className="sidebar">
+      <div className="max-w-[1580px] p-[24px] pb-0">
+        <div className="flex gap-[20px]">
+          <div className="flex flex-col gap-[24px] pt-[24px] min-w-[180px]">
             <a>주문조회</a>
             <a>1:1 문의</a>
             <a>위시리스트</a>
@@ -16,38 +15,47 @@ function Myreview() {
             <a>회원탈퇴</a>
           </div>
 
-          <div className="main">
-            <div className="section top">
+          <div className="flex-grow min-w-0 basis-0 flex flex-col gap-[20px]">
+            <div className="flex items-start p-[30px] text-[16px] border-t-2 border-black gap-[18px]">
               <img src="src/pages/user/icons/basket.svg" />
-              <div className="first-depth">
-                <h1>상품리뷰</h1>
+              <div className="flex flex-col gap-[16px]">
+                <h1 className="text-[26px] font-gowunBold">상품리뷰</h1>
                 <p>상품의 품질에 대해서 얼마나 만족하시나요?</p>
               </div>
             </div>
-            <div className="section second">
+            <div className="flex items-start p-[30px] text-[16px] border-t border-grey-30 gap-[18px]">
               <img src="src/pages/user/icons/image.png" />
-              <div className="second-depth">
-                <p className="product-name">안티 헤어 로스 샴푸바</p>
+              <div className="flex flex-col gap-[20px]">
+                <p className="font-gowunBold">안티 헤어 로스 샴푸바</p>
                 <p>12,900원 - 1개</p>
               </div>
             </div>
-            <div className="section">
-              <h2 className="fix">상세리뷰</h2>
-              <div className="field">
+            <div className="flex items-start p-[30px] text-[16px] border-t border-grey-30 gap-[60px]">
+              <h2 className="flex-shrink-0 font-gowunBold text-[20px]">
+                상세리뷰
+              </h2>
+              <div className="flex-grow border border-grey-20 rounded-[2px] w-full">
                 <textarea
+                  className="block w-full p-[10px] box-border text-[14px] placeholder:text-grey-40"
                   rows="10"
                   placeholder="다른 고객님에게 도움이 되도록 상품에 대한 솔직한 평가를 남겨주세요."
                 ></textarea>
               </div>
             </div>
-            <div className="section">
-              <h2>사진첨부</h2>
-              <button className="button">사진 첨부하기</button>
+            <div className="flex items-start p-[30px] text-[16px] border-t border-grey-30 gap-[60px]">
+              <h2 className="font-gowunBold text-[20px]">사진첨부</h2>
+              <button className="font-gowunBold h-[42px] leading-[42px] text-primary-70 text-[16px] px-[24px] border-2 border-primary-30 rounded-[2px] box-border cursor-pointer">
+                사진 첨부하기
+              </button>
             </div>
 
-            <div className="btn-section">
-              <button className="btn btn-cancel">취소</button>
-              <button className="btn btn-register">등록</button>
+            <div className="flex justify-center gap-[24px] border-t border-grey-30 p-[34px]">
+              <button className="bg-grey-40 inline-block text-[16px] text-white h-[48px] leading-[48px] font-gowunBold box-border cursor-pointer rounded-[12px] px-[64px]">
+                취소
+              </button>
+              <button className="bg-primary-40 inline-block text-[16px] text-white h-[48px] leading-[48px] font-gowunBold box-border cursor-pointer rounded-[12px] px-[64px]">
+                등록
+              </button>
             </div>
           </div>
         </div>
