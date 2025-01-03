00551bc4f5d279fbef46da019d09ea0ebf9d4d49
jiyoungnim <cloundyon31@gmail.com>
2025-01-03 13:38:18 +0900

    📝 이미지 파일 구하러 출발..

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,8 +1,10 @@
 import Login from "@pages/user/Login";
+import Signup from "@pages/user/Signup";
 
 const homeRoutes = [
   // { index: true, element: <mainLayout /> },
   { path: "login", element: <Login /> },
+  { path: "signup", element: <Signup /> },
 ];
 
 export default homeRoutes;
