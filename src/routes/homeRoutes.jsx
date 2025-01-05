f49128f6ef7447217641d2f77065bfd7338a5137
jiyoungnim <cloundyon31@gmail.com>
2025-01-05 16:20:50 +0900

    📝 tailwind CSS 수정중...

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,8 +1,10 @@
 import Login from "@pages/user/Login";
+import TermsOfService from "@pages/user/TermsOfService";
 
 const homeRoutes = [
   // { index: true, element: <mainLayout /> },
   { path: "login", element: <Login /> },
+  { path: "tos", element: <TermsOfService /> },
 ];
 
 export default homeRoutes;
