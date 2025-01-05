e2cd5af972d7ace72f5dce9ce78aeada05dce244
jiyoungnim <cloundyon31@gmail.com>
2025-01-05 16:55:35 +0900

    📝 tailwind CSS 수정중... 체크박스 부분 오류 발생

--- src/pages/user/TermsOfService.jsx
+++ src/pages/user/TermsOfService.jsx
@@ -1,4 +1,3 @@
-import "./main.css";
 import "./jy-global.css";
 
 function TermsOfService() {
@@ -7,10 +6,10 @@ function TermsOfService() {
       <div className="my-[60px] mx-auto max-w-[1000px] flex flex-col gap-[20px]">
         <form className="flex flex-col gap-[32px] text-[18px]">
           <div className="flex">
-            <input type="checkbox" id="flex gap-2 cursor-pointer items-end" />
+            <input type="checkbox" id="check" className="peer" />
             <label
-              className="flex gap-2 cursor-pointer items-end"
-              htmlFor="flex gap-2 cursor-pointer items-end"
+              className="flex gap-4 cursor-pointer items-end before:w-[20px] before:h-[20px] before:inline-block before:content-[''] before:bg-[url('./icons/emptybox.svg')] peer-checked:before:bg-[url('./icons/checkbox.svg')] font-gowunBold"
+              htmlFor="check"
             >
               전체 동의하기
             </label>
@@ -18,19 +17,23 @@ function TermsOfService() {
 
           <div>
             <div className="flex">
-              <input type="checkbox" id="termsService" />
+              <input type="checkbox" id="termsService" className="peer" />
               <label
-                className="flex gap-2 cursor-pointer items-end"
+                className="flex gap-4 cursor-pointer items-end before:w-[20px] before:h-[20px] before:inline-block before:content-[''] before:bg-[url('./icons/emptybox.svg')] peer-checked:before:bg-[url('./icons/checkbox.svg')] font-gowunBold"
                 htmlFor="termsService"
               >
                 이용약관 동의
-                <em className="option point">(필수)</em>
+                <em className="inline-block font-gowunBold text-[14px] text-error">
+                  (필수)
+                </em>
               </label>
             </div>
-            <div className="terms_box">
-              <div className="article">
-                <h1>여러분을 환영합니다.</h1>
-                <p className="article-text">
+            <div className="border border-[#ddd] mt-6 ml-8 rounded-lg p-[18px] max-h-[100px] overflow-auto box-border text-[15px] tracking-wide ">
+              <div className="mb-4">
+                <h1 className="text-[18px] font-gowunBold">
+                  여러분을 환영합니다.
+                </h1>
+                <p className="mt-[10px] leading-[20px]">
                   다시봄 서비스 및 제품(이하 ‘서비스’)을 이용해 주셔서
                   감사합니다. 본 약관은 다양한 다시봄 서비스의 이용과 관련하여
                   다시봄 서비스를 제공하는 다시봄 주식회사(이하 ‘다시봄’)와 이를
@@ -43,9 +46,11 @@ function TermsOfService() {
                 </p>
               </div>
 
-              <div className="article">
-                <h1>다양한 다시봄 서비스를 즐겨보세요.</h1>
-                <p className="article-text">
+              <div className="mb-4">
+                <h1 className="text-[18px] font-gowunBold">
+                  다양한 다시봄 서비스를 즐겨보세요.
+                </h1>
+                <p className="mt-[10px] leading-[20px]">
                   다시봄는 www.naver.com을 비롯한 다시봄 도메인의 웹사이트 및
                   응용프로그램(어플리케이션, 앱)을 통해 정보 검색, 다른
                   이용자와의 커뮤니케이션, 콘텐츠 제공, 상품 쇼핑 등 여러분의
@@ -62,37 +67,41 @@ function TermsOfService() {
 
           <div>
             <div className="flex">
-              <input type="checkbox" id="termsPrivacy" />
+              <input type="checkbox" id="termsPrivacy" className="peer" />
               <label
-                className="flex gap-2 cursor-pointer items-end"
+                className="flex gap-4 cursor-pointer items-end before:w-[20px] before:h-[20px] before:inline-block before:content-[''] before:bg-[url('./icons/emptybox.svg')] peer-checked:before:bg-[url('./icons/checkbox.svg')] font-gowunBold"
                 htmlFor="termsPrivacy"
               >
                 개인정보 수집 및 이용 동의
-                <em className="option point">(필수)</em>
+                <em className="inline-block font-gowunBold text-[14px] text-error">
+                  (필수)
+                </em>
               </label>
             </div>
-            <div className="terms_box">
-              <div className="article">
-                <h1>1. 수집하는 개인정보</h1>
-                <p className="article-text">
+            <div className="border border-[#ddd] mt-6 ml-8 rounded-lg p-[18px] max-h-[100px] overflow-auto box-border text-[15px] tracking-wide">
+              <div className="mb-4">
+                <h1 className="text-[18px] font-gowunBold">
+                  1. 수집하는 개인정보
+                </h1>
+                <p className="mt-[10px] leading-[20px]">
                   개인정보보호법에 따라 다시봄에 회원가입 신청하시는 분께
                   수집하는 개인정보의 항목, 개인정보의 수집 및 이용목적,
                   개인정보의 보유 및 이용기간, 동의 거부권 및 동의 거부 시
                   불이익에 관한 사항을 안내 드리오니 자세히 읽은 후 동의하여
                   주시기 바랍니다.
                 </p>
-                <p className="article-text">
+                <p className="mt-[10px] leading-[20px]">
                   이용자는 회원가입을 하지 않아도 정보 검색, 뉴스 보기 등
                   대부분의 다시봄 서비스를 회원과 동일하게 이용할 수 있습니다.
                   이용자가 메일, 캘린더, 카페, 블로그 등과 같이 개인화 혹은
                   회원제 서비스를 이용하기 위해 회원가입을 할 경우, 다시봄는
                   서비스 이용을 위해 필요한 최소한의 개인정보를 수집합니다.
                 </p>
-                <p className="article-text">
+                <p className="mt-[10px] leading-[20px]">
                   회원가입 시점에 다시봄이 이용자로부터 수집하는 개인정보는
                   아래와 같습니다.
                 </p>
-                <p className="article-text break">
+                <p className="mt-[10px] leading-[20px] block">
                   - 회원 가입 시 필수항목으로 아이디, 비밀번호, 이름, 생년월일,
                   성별, 휴대전화번호를, 선택항목으로 본인확인 이메일주소를
                   수집합니다. 실명 인증된 아이디로 가입 시, 암호화된 동일인
@@ -104,19 +113,19 @@ function TermsOfService() {
                   휴대전화번호(아이핀 인증인 경우 아이핀번호))를 추가로
                   수집합니다.
                 </p>
-                <p className="article-text break">
+                <p className="mt-[10px] leading-[20px] block">
                   - 비밀번호 없이 회원 가입 시에는 필수항목으로 아이디, 이름,
                   생년월일, 휴대전화번호를, 선택항목으로 비밀번호를 수집합니다.{" "}
                 </p>
-                <p className="article-text">
+                <p className="mt-[10px] leading-[20px]">
                   서비스 이용 과정에서 이용자로부터 수집하는 개인정보는 아래와
                   같습니다.
-                  <p className="article-text break">
+                  <p className="mt-[10px] leading-[20px] block">
                     - 회원정보 또는 개별 서비스에서 프로필 정보(별명, 프로필
                     사진)를 설정할 수 있습니다. 회원정보에 별명을 입력하지 않은
                     경우에는 마스킹 처리된 아이디가 별명으로 자동 입력됩니다.
                   </p>
-                  <p className="article-text break">
+                  <p className="mt-[10px] leading-[20px] block">
                     - 다시봄 내의 개별 서비스 이용, 이벤트 응모 및 경품 신청
                     과정에서 해당 서비스의 이용자에 한해 추가 개인정보 수집이
                     발생할 수 있습니다. 추가로 개인정보를 수집할 경우에는 해당
@@ -130,32 +139,34 @@ function TermsOfService() {
           </div>
 
           <div className="flex flex-col gap-[30px]">
-            <label className="flex gap-2 cursor-pointer items-end font-gowunBold">
+            <label className="flex gap-4 items-end font-gowunBold">
               마케팅 활용 동의 및 광고 수신 동의
-              <em className="option">(선택)</em>
+              <em className="inline-block font-gowunBold text-[14px] text-grey-50">
+                (선택)
+              </em>
             </label>
-            <div className="termsMarketing">
-              <input type="checkbox" id="termsSMS" />
+            <div className="flex flex-col gap-6 text-[16px]">
+              <input type="checkbox" id="termsSMS" className="peer" />
               <label
-                className="flex gap-2 cursor-pointer items-end"
+                className="flex gap-4 items-center cursor-pointer before:w-[20px] before:h-[20px] before:inline-block before:content-[''] before:bg-[url('./icons/emptybox.svg')] peer-checked:before:bg-[url('./icons/checkbox.svg')]"
                 htmlFor="termsSMS"
               >
                 SMS 수신 동의
               </label>
-              <input type="checkbox" id="termsEmail" />
+              <input type="checkbox" id="termsEmail" className="peer" />
               <label
-                className="flex gap-2 cursor-pointer items-end"
+                className="flex gap-4 items-center cursor-pointer before:w-[20px] before:h-[20px] before:inline-block before:content-[''] before:bg-[url('./icons/emptybox.svg')] peer-checked:before:bg-[url('./icons/checkbox.svg')]"
                 htmlFor="termsEmail"
               >
                 E-mail 수신 동의
               </label>
-              <div className="terms_box small">
-                <div className="article">
-                  <p className="article-text small">
+              <div className="border border-[#ddd] mt-6 ml-8 rounded-lg p-[18px] max-h-[100px] overflow-auto box-border text-[15px] tracking-wide">
+                <div>
+                  <p className="leading-[20px]">
                     서비스와 관련된 소식, 이벤트 안내, 고객 혜택 등 다양한
                     정보를 제공합니다.
                   </p>
-                  <p className="article-text small">
+                  <p className="leading-[20px]">
                     지구샵에서 진행하는 행사 소식을 가장 빠르게 받아보실 수
                     있습니다!
                   </p>
@@ -163,9 +174,13 @@ function TermsOfService() {
               </div>
             </div>
           </div>
-          <div className="btn-wrap">
-            <button className="btn btn-cancel">취소</button>
-            <button className="btn btn-register">등록</button>
+          <div className="flex justify-center gap-[24px] p-[30px]">
+            <button className="bg-grey-40 inline-block text-4 text-white h-[48px] leading-[48px] px-[64px] font-gowunBold box-border cursor-pointer rounded-[12px]  btn-cancel">
+              취소
+            </button>
+            <button className="bg-primary-40 inline-block text-4 text-white h-[48px] leading-[48px] px-[64px] font-gowunBold box-border cursor-pointer rounded-[12px] btn-register">
+              등록
+            </button>
           </div>
         </form>
       </div>
