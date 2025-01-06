62e9e945cb9a321c9fd65bce709db625bf3d78dc
ohDASEUL <comeseul312@gmail.com>
2025-01-06 18:24:03 +0900

    🚚 라우터 경로 수정

--- src/routes/homeRoutes.jsx
+++ src/routes/homeRoutes.jsx
@@ -1,12 +1,13 @@
 import Layout from '@components/layouts';
 import Slider from '@pages/Slider';
 import Login from '@pages/user/Login';
-import Layout from '@components/layouts';
-import Slider from '@pages/Slider';
-import Login from '@pages/user/Login';
 import Myreview from '@pages/user/Myreview';
 import Shop from '@pages/Shop';
 import Detail from '@pages/Detail';
+import ListPage from '@pages/QnA/ListPage';
+import NewPostPage from '@pages/QnA/NewPostPage';
+import EditPostPage from '@pages/QnA/EditPostPage';
+import PostDetailPage from '@pages/QnA/PostDetailPage';
 
 const homeRoutes = [
   {
@@ -14,10 +15,17 @@ const homeRoutes = [
     element: <Layout />,
     children: [
       { index: true, element: <Slider /> },
+
       { path: 'login', element: <Login /> },
       { path: 'review', element: <Myreview /> },
+
       { path: 'shop', element: <Shop /> },
       { path: 'detail', element: <Detail /> },
+
+      { path: 'qna', element: <ListPage /> },
+      { path: 'qna/new', element: <NewPostPage /> },
+      { path: 'qna/edit', element: <EditPostPage /> },
+      { path: 'qna/detail', element: <PostDetailPage /> },
     ],
   },
 ];
