c82c3f82e191a4bef31c2b77493e2b34de227dd0
ohDASEUL <comeseul312@gmail.com>
2025-01-06 10:43:20 +0900

    🧪 [헤더] 드롭다운 필요없는 메뉴는 드롭다운 안 되게       [홈] "/" 에서만 이벤트 슬라이더 보이게

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,26 @@
-import Header from "@components/layouts/Header";
-import Footer from "@components/layouts/Footer";
-import { Outlet } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
-import Slider from "@components/Slider";
+import Header from '@components/layouts/Header';
+import Footer from '@components/layouts/Footer';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import Slider from '@components/Slider';
 
 const Home = () => {
+  const location = useLocation();
+  const isMainPage = location.pathname === '/';
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       <Helmet>
         <title>다시, 봄 홈페이지</title>
       </Helmet>
       {/* Header */}
       <Header />
 
-      {/* Slider */}
-      <Slider />
+      {/* Slider - only show on main page */}
+      {isMainPage && <Slider />}
 
       {/* Main Content */}
-      <main className="flex-grow container mx-auto px-4 py-8">
+      <main className='flex-grow container mx-auto px-4 py-8'>
         <Outlet />
       </main>
 
