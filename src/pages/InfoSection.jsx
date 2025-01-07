0be537a94e4a20f7973f092948f3acf8821a26aa
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-01-07 16:31:00 +0900

    공지사항, 문의사항 더미데이터 JSON 배열 형식으로 리펙토링

--- src/pages/InfoSection.jsx
+++ src/pages/InfoSection.jsx
@@ -1,13 +1,22 @@
 const InfoSection = () => {
-  const notices = Array.from({ length: 7 }, (_, i) => ({
-    title: `공지사항 ${i + 1}`,
-    date: "24.01.01",
-  }));
 
-  const inquiries = Array.from({ length: 5 }, (_, i) => ({
-    title: `문의사항 ${i + 1}`,
-    date: "24.01.01",
-  }));
+  const notices = [
+    { id: 1, title: "공지사항 1", date: "24.01.01" },
+    { id: 2, title: "공지사항 2", date: "24.01.02" },
+    { id: 3, title: "공지사항 3", date: "24.01.03" },
+    { id: 4, title: "공지사항 4", date: "24.01.04" },
+    { id: 5, title: "공지사항 5", date: "24.01.05" },
+  ];
+
+  const inquiries = [
+    { id: 1, title: "문의사항 1", date: "24.01.01" },
+    { id: 2, title: "문의사항 2", date: "24.01.02" },
+    { id: 3, title: "문의사항 3", date: "24.01.03" },
+    { id: 4, title: "문의사항 4", date: "24.01.04" },
+    { id: 5, title: "문의사항 5", date: "24.01.05" },
+  ];
+
+
 };
 
 export default InfoSection;
