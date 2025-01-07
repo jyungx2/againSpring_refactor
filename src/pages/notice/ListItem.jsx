105a9a3edbc5df9737b6c7fcf79879154a8e826e
ohDASEUL <comeseul312@gmail.com>
2025-01-07 10:55:51 +0900

    💄 [공지사항_목록] 공지사항을 알려주는 라벨? 추가

--- src/pages/notice/ListItem.jsx
+++ src/pages/notice/ListItem.jsx
@@ -7,6 +7,7 @@ export default function ListItem({ title, date }) {
       <div className='rounded w-[280px] h-[280px] bg-grey-10' />
       <div className='pt-[19px] pl-[10px] w-full'>
         <h3 className='text-xl my-2 mx-0'>
+          <strong className='text-red-400 mr-3'>공지사항</strong>
           <Link
             to='detail'
             className='hover:text-secondary-20 transition-colors'
@@ -14,7 +15,7 @@ export default function ListItem({ title, date }) {
             {title}
           </Link>
         </h3>
-        <h4 className='text-grey-50 text-lg m-0'>{date}</h4>
+        <h4 className='text-grey-70 text-lg m-0'>{date}</h4>
       </div>
     </div>
   );
