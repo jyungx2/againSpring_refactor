4db2d8ff2a5761ac561c08ca0ee482093fb204dd
ohDASEUL <comeseul312@gmail.com>
2025-01-07 17:44:14 +0900

    💄 [상품Q&A_수정,상세] 마크업

---
+++ src/pages/qna/ProductQnAPostDetailPage.jsx
@@ -0,0 +1,223 @@
+import { Link, useNavigate } from 'react-router-dom';
+import '../../assets/styles/fonts.css';
+import withReactContent from 'sweetalert2-react-content';
+import Swal from 'sweetalert2';
+
+export default function ProductQnAPostDetailPage() {
+  const MySwal = withReactContent(Swal);
+  const navigate = useNavigate();
+
+  const deleteCheckBtn = () => {
+    MySwal.fire({
+      title: '게시글을 삭제하시겠습니까?',
+      text: '삭제된 게시글은 복구할 수 없습니다.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '네',
+      cancelButtonText: '아니요',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        MySwal.fire({
+          title: '삭제 완료',
+          text: '게시글이 삭제되었습니다.',
+          confirmButtonText: '확인',
+          icon: 'success',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate('/qna');
+          }
+        });
+      }
+    });
+  };
+
+  return (
+    <div className='w-[1200px] mx-auto px-6 py-4'>
+      <h1 className='h-[80px] text-4xl text-center box-border m-0 px-0 py-[20px]'>
+        Q&amp;A
+      </h1>
+
+      {/* 상품 정보 불러오기 */}
+      <div className='flex items-center mb-4 p-6 border rounded-md w-full'>
+        <div className='mr-6'>
+          <div className='w-32 h-32 bg-gray-200 flex items-center justify-center text-sm text-gray-600'>
+            상품 Image
+          </div>
+        </div>
+        <div className='flex flex-col gap-4 justify-center h-32'>
+          <div className='text-lg'>
+            상품명: 대나무 칫솔 (소형) <br /> 1,400원
+          </div>
+          <div className='flex gap-4'>
+            <button className='px-6 py-2.5 bg-black text-white text-base rounded hover:bg-gray-800'>
+              <Link to='/detail'>상품상세보기</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <section className='flex flex-col'>
+        {/* 게시글 헤더 */}
+        <div className='border-t border-black'>
+          <div className='flex items-center gap-[100px] py-4 border-b border-grey-10'>
+            <label
+              className='text-lg font-medium text-grey-80 w-24'
+              htmlFor='title'
+            >
+              제목
+            </label>
+            <h2 className='text-xl font-medium text-grey-50' id='title'>
+              상품 관련 문의
+            </h2>
+          </div>
+          <div className='flex items-center gap-[100px] py-4 border-b border-grey-10'>
+            <label
+              className='text-lg font-medium text-grey-80 w-24'
+              htmlFor='writer'
+            >
+              작성자
+            </label>
+            <p className='text-xl font-medium text-grey-50' id='writer'>
+              홍길동
+            </p>
+          </div>
+          <div className='border-b border-grey-10'>
+            <div className='flex gap-[43px] py-4'>
+              <div className='flex items-center'>
+                <label className='text-lg font-medium pl-5 mr-2' htmlFor='date'>
+                  작성일
+                </label>
+                <p className='text-lg text-grey-40' id='date'>
+                  2024-01-01 00:00:00
+                </p>
+              </div>
+              <div className='flex items-center'>
+                <label className='text-lg font-medium mr-2' htmlFor='views'>
+                  조회수
+                </label>
+                <p className='text-lg text-grey-40' id='views'>
+                  0
+                </p>
+              </div>
+            </div>
+
+            {Array.from({ length: 10 }, (_, i) => (
+              <p key={i} className='py-4 text-lg'>
+                여기에 글 내용이 들어갑니다 {i + 1}번째 줄
+              </p>
+            ))}
+          </div>
+        </div>
+
+        {/* 댓글 섹션 */}
+        <section className='mb-8'>
+          <div className='py-8'>
+            <div className='flex items-center'>
+              <label className='text-xl font-medium' htmlFor='admin'>
+                관리자
+              </label>
+              <p className='text-xl text-grey-50 font-normal ml-3' id='admin'>
+                2024-01-01-01 00:00:00
+              </p>
+            </div>
+            <p className='text-lg text-grey-80 mt-4'>내일 출고 예정 입니다.</p>
+            <div className='flex mt-4'>
+              <button
+                type='button'
+                className='text-xl text-grey-40 hover:text-grey-70 font-normal'
+              >
+                댓글
+              </button>
+              <button
+                type='button'
+                className="text-xl text-grey-40  hover:text-grey-70 font-normal relative ml-4 before:content-['/'] before:absolute before:left-[-8px]"
+              >
+                수정
+              </button>
+              <button
+                type='button'
+                className="text-xl text-grey-40 hover:text-grey-70 font-normal relative ml-4 before:content-['/'] before:absolute before:left-[-8px]"
+              >
+                지우기
+              </button>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-4 border border-grey-5 p-6 mb-6'>
+            <textarea
+              className='w-full min-h-[80px] resize-y border border-grey-30 p-2'
+              placeholder='관리자만 작성하실 수 있습니다.'
+              id='reply-text'
+            />
+            <div className='flex justify-end'>
+              <button
+                type='submit'
+                className='rounded-lg bg-secondary-20 text-white px-6 py-2'
+              >
+                작성
+              </button>
+            </div>
+          </div>
+        </section>
+
+        {/* 하단 네비게이션 */}
+        <div className='border-t border-grey-10 pt-8 pb-4'>
+          <div className='flex justify-between mb-5'>
+            <button
+              type='button'
+              className='border border-grey-10 rounded px-9 py-2'
+            >
+              <Link to='/qna'>목록</Link>
+            </button>
+            <div className='flex gap-3'>
+              <button
+                type='button'
+                className='border border-grey-10 rounded px-9 py-2'
+              >
+                <Link to='/qna/product/edit'>수정</Link>
+              </button>
+              <button
+                type='button'
+                className='border border-grey-10 rounded px-9 py-2'
+                onClick={() => {
+                  deleteCheckBtn();
+                }}
+              >
+                삭제
+              </button>
+            </div>
+          </div>
+
+          <nav className='mb-4'>
+            <div className='border-t border-b border-grey-5'>
+              <div className='flex items-center border-b border-grey-5 min-h-[60px]'>
+                <div className='w-[100px] sm:w-[120px] px-4 py-4 text-grey-50 text-lg font-medium shrink-0'>
+                  <span className='text-sm mr-2'>▲</span>이전글
+                </div>
+                <Link
+                  to='#'
+                  className='flex-1 px-4 py-4 text-lg text-grey-80 hover:text-secondary-20 truncate'
+                >
+                  이전 공지글
+                </Link>
+              </div>
+              <div className='flex items-center min-h-[60px]'>
+                <div className='w-[100px] sm:w-[120px] px-4 py-4 text-grey-50 text-lg font-medium shrink-0'>
+                  <span className='text-sm mr-2'>▼</span>다음글
+                </div>
+                <Link
+                  to='#'
+                  className='flex-1 px-4 py-4 text-lg text-grey-80 hover:text-secondary-20 truncate'
+                >
+                  다음 공지글
+                </Link>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </section>
+    </div>
+  );
+}
