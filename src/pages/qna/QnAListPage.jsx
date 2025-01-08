aaac731e75a341c21ac491048c8571e01244527a
ohDASEUL <comeseul312@gmail.com>
2025-01-08 17:48:30 +0900

    👔 [Q&A_목록] 질문하기 버튼 클릭 시 비 로그인 상태이면 로그인 페이지로 이동

--- src/pages/qna/QnAListPage.jsx
+++ src/pages/qna/QnAListPage.jsx
@@ -1,8 +1,17 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import useUserStore from '@store/userStore';
 import '../../assets/styles/fonts.css';
 import ListItem from './QnAListItem';
+import { useNavigate } from 'react-router-dom';
+import withReactContent from 'sweetalert2-react-content';
+import Swal from 'sweetalert2';
 
 export default function QnAListPage() {
+  const user = useUserStore();
+  const navigate = useNavigate();
+
+  const MySwal = withReactContent(Swal);
+
   // 실제로는 API에서 받아올 데이터
   const items = [
     {
@@ -21,19 +30,60 @@ export default function QnAListPage() {
     },
   ];
 
+  /**
+   * TODO 비 로그인 상태이면 로그인 페이지로 이동
+   * 1. userStore import 하기(완료)
+   * 2. useUserStore hook으로부터 user 상태 가져오기(완료)
+   * 3. onClick 핸들러 함수 작성하기(완료)
+   * 4. Link 컴포넌트를 button으로 변경하기(완료)
+   * 5. button에 onClick 핸들러 연결하기(완료)
+   * 6. navigate import 하기(완료)
+   * 7. useNavigate hook 사용(아직 구현 안 함)
+   */
+
+  const questionButton = () => {
+    if (!user) {
+      MySwal.fire({
+        title: '로그인이 필요합니다.',
+        text: ' 로그인 페이지로 이동하시겠습니까?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '네',
+        cancelButtonText: '아니요',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          MySwal.fire({
+            title: '알림',
+            text: '로그인 페이지로 이동합니다',
+            confirmButtonText: '확인',
+            icon: 'info',
+          }).then((result) => {
+            if (result.isConfirmed) {
+              navigate('/login');
+            }
+          });
+        }
+      });
+    } else {
+      navigate('/qna/new');
+    }
+  };
+
   return (
     <div className='w-[1200px] mx-auto px-6 mb-20'>
       <h1 className='h-[80px] text-4xl text-center box-border m-0 px-0 py-[20px]'>
-        Q&amp;A
+        Q&A
       </h1>
 
       <div className='flex justify-end mb-5 w-full'>
-        <Link
-          to='/qna/new'
+        <button
+          onClick={questionButton}
           className='px-5 py-2 bg-secondary-20 text-white rounded hover:bg-secondary-40 transition-colors'
         >
           질문하기
-        </Link>
+        </button>
       </div>
       <div className='w-full mx-auto my-0 max-h-[906.11px] overflow-y-auto'>
         <table className='w-full border-collapse table-fixed'>
