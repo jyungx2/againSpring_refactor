32f696f0adf5c1b4064017214c7641d4ab18c763
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-01-08 14:10:11 +0900

    문의사항 -> Q&A 텍스트 변경

--- src/pages/InfoSection.jsx
+++ src/pages/InfoSection.jsx
@@ -48,14 +48,14 @@ const InfoSection = () => {
             </ul>
           </div>
 
-          {/* 문의사항 */}
+          {/* Q&A */}
           <div className="border-b border-gray-300 rounded-md overflow-hidden">
             <div className="flex justify-between items-center py-4 px-6 border-b border-gray-300">
-              <h3 className="text-3xl font-bold">문의사항</h3>
-              <Link to="/inquiries">
+              <h3 className="text-3xl font-bold">Q&A</h3>
+              <Link to="/QnA">
                 <button
                   className="w-8 h-8 hover:opacity-80 focus:outline-none"
-                  aria-label="문의사항 더보기"
+                  aria-label="Q&A 더보기"
                 >
                   <img src="/icons/plus.svg" alt="더보기" />
                 </button>
