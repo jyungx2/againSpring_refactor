99e0af82350f86bcc85e333dfb9b41e93c0eb56f
ohDASEUL <comeseul312@gmail.com>
2025-01-11 22:41:21 +0900

    ✨ [게시판_댓글] 댓글 등록 컴포넌트 구현 완료

--- src/pages/comment/CommentNew.jsx
+++ src/pages/comment/CommentNew.jsx
@@ -1,21 +1,71 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import useUserStore from '@store/userStore';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
-/**
- * TODO 댓글 등록
- * - 새 댓글 작성 폼
- * - 댓글 개수 표시
- * - 사용자 권한에 따른 UI 처리
- */
-const CommentNew = ({ postId, isAdmin = false }) => {
+const CommentNew = ({ isAdmin, post, comments, setReplies }) => {
+  const { user } = useUserStore();
   const [content, setContent] = useState('');
+  const MySwal = withReactContent(Swal);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!content.trim()) return;
 
+    if (!isAdmin && post?.user?._id != user?._id) {
+      MySwal.fire({
+        title: '권한 없음',
+        text: '답변은 작성자와 관리자만 댓글을 작성할 수 있습니다.',
+        icon: 'warning',
+        confirmButtonText: '확인',
+      });
+      return;
+    }
+
+    if (!content.trim()) {
+      MySwal.fire({
+        title: '알림',
+        text: '댓글 내용을 입력해주세요.',
+        icon: 'warning',
+        confirmButtonText: '확인',
+      });
+      return;
+    }
+
+    console.log('tt');
     // API 호출 로직
-    setContent('');
+
+    const data = {
+      content: content,
+    };
+
+    const saveData = async () => {
+      // 서버에 게시글 저장 요청
+      const response = await fetch(
+        `https://11.fesp.shop/posts/${post._id}/replies`,
+        {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'client-id': 'final02',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        }
+      );
+
+      /**
+       * 리액트 새로고침 되는 기준 useState로 관리되는 모든 자원들의 값이 변화가 있으면
+       * 리액트에서는 부분 새로고침이 일어남
+       * 그래서 useState로 replies를 관리하게 해서 강제로 새로고침을 일어나게함
+       */
+      const result = await response.json();
+      if (result.item && post.replies) {
+        setReplies([...comments, result.item]);
+      }
+    };
+
+    saveData();
   };
 
   return (
@@ -23,9 +73,12 @@ const CommentNew = ({ postId, isAdmin = false }) => {
       <textarea
         className='w-full min-h-[80px] resize-y border border-grey-30 p-2 text-xl'
         placeholder={
-          isAdmin ? '댓글을 입력하세요' : '답변은 관리자만 작성할 수 있습니다.'
+          // 관리자도 아니고 작성자도 아니라면 작성권한 없음
+          !isAdmin && post?.user?._id != user?._id
+            ? '답변은 작성자와 관리자만 작성할 수 있습니다.'
+            : '댓글을 입력하세요'
         }
-        disabled={!isAdmin}
+        disabled={!isAdmin && post?.user?._id != user?._id}
         value={content}
         onChange={(e) => setContent(e.target.value)}
       />
@@ -33,9 +86,11 @@ const CommentNew = ({ postId, isAdmin = false }) => {
         <button
           type='submit'
           onClick={handleSubmit}
-          disabled={!isAdmin}
+          disabled={!isAdmin && post?.user?._id != user?._id}
           className={`rounded-lg px-6 py-2 ${
-            isAdmin ? 'bg-secondary-20' : 'bg-grey-20'
+            !isAdmin && post?.user?._id != user?._id
+              ? 'bg-grey-20'
+              : 'bg-secondary-20'
           } text-white`}
         >
           작성
