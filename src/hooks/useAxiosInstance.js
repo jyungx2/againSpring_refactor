84c26625eb404a9fe8031f65d4259082724cadb1
jiyoungnim <cloundyon31@gmail.com>
2025-01-11 19:31:05 +0900

    📝 요청 인터셉처 작성 완료 (config.headers.Authorization에 accessToken 설정)

--- src/hooks/useAxiosInstance.js
+++ src/hooks/useAxiosInstance.js
@@ -1,14 +1,31 @@
 import axios from "axios";
+import useUserStore from "@store/userStore";
+
+const REFRESH_url = "/auth/refresh";
 
 function useAxiosInstance() {
+  const { user } = useUserStore();
+
   const instance = axios.create({
-    baseURL: "https://11.fesp.shop",
+    baseURL: "https://11.fesp.sho",
     timeout: 1000 * 15,
     headers: {
       "Content-Type": "application/json",
       accept: "application/json",
       "client-id": "final02",
     },
+  });
+
+  instance.interceptors.request.use((config) => {
+    if (user && config.url !== REFRESH_url) {
+      config.headers.Authorization = `Bearer ${user.accessToken}`;
+    }
+
+    config.params = {
+      delay: 1000,
+      ...config.params,
+    };
+    return config;
   });
 
   return instance;
