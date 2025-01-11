e296841f21fe4ba4486b46c95106e743f60c7e37
ohDASEUL <comeseul312@gmail.com>
2025-01-11 14:42:19 +0900

    🚧 [게시판_댓글] 댓글 목록 구현 중

--- src/pages/comment/CommentList.jsx
+++ src/pages/comment/CommentList.jsx
@@ -1,11 +1,36 @@
+import CommentListItem from '@pages/comment/CommentListItem';
+import CommentNew from '@pages/comment/CommentNew';
+import PropTypes from 'prop-types';
+
 /**
  * TODO 댓글목록
  * - comments 배열을 props로 받아서 CommentListItem을 반복 렌더링
  * - 댓글 개수 표시
  * - CommentNew 컴포넌트 포함
  */
-const CommentList = () => {
-  return <div>댓글목록</div>;
+const CommentList = ({ comments = [], postId, isAdmin = false }) => {
+  return (
+    <section className='mb-8'>
+      <h4 className='mt-8 mb-4 ml-2'>댓글 {comments.length}</h4>
+      {comments.map((comment) => (
+        <CommentListItem key={comment.id} comment={comment} postId={postId} />
+      ))}
+      <CommentNew postId={postId} isAdmin={isAdmin} />
+    </section>
+  );
 };
 
 export default CommentList;
+
+CommentList.propTypes = {
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      content: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  postId: PropTypes.string.isRequired,
+  isAdmin: PropTypes.bool,
+};
