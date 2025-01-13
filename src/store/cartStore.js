1560882a63350f2cf80324e878bf777cdbfe888a
khk99 <khk990815@gmail.com>
2025-01-13 11:18:19 +0900

    ✨ 장바구니 수량 변경 상태 업데이트

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -2,14 +2,25 @@ import { create } from "zustand";
 import useAxiosInstance from "../hooks/useAxiosInstance";
 import useUserStore from "@store/userStore";
 
-export const cartStore = create((set) => {
+export const cartStore = create((set, get) => {
   const axiosInstance = useAxiosInstance();
+
+  const computeTotalOrderAmount = () => {
+    const { cartItemsList, shippingCost } = get();
+    const totalPrice = cartItemsList.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+    return totalPrice + shippingCost;
+  };
 
   return {
     cartItemsList: [],
     shippingCost: 0,
+    totalOrderAmount: 0,
     loading: false,
     error: null,
+
     fetchCartItems: async () => {
       set({ loading: true, error: null });
 
@@ -33,6 +44,7 @@ export const cartStore = create((set) => {
         set({
           cartItemsList: products,
           shippingCost: response.data.cost.shippingFees,
+          totalOrderAmount: computeTotalOrderAmount(),
           loading: false,
         });
       } catch (error) {
@@ -45,6 +57,19 @@ export const cartStore = create((set) => {
           error: "장바구니 아이템을 가져오는 데 실패했습니다.",
         });
       }
+    },
+
+    updateItemQuantity: (productId, newQuantity) => {
+      const { cartItemsList } = get();
+
+      const updatedCartItemsList = cartItemsList.map((item) =>
+        item.id === productId ? { ...item, quantity: newQuantity } : item
+      );
+
+      set({
+        cartItemsList: updatedCartItemsList,
+        totalOrderAmount: computeTotalOrderAmount(),
+      });
     },
   };
 });
