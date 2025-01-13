8b8342a6efd8351453a5748e30d9a388bd67a7a1
ohDASEUL <comeseul312@gmail.com>
2025-01-14 03:26:22 +0900

    ♻️ [Q&A_목록] 상품 Q&A 등록 시 목록에서도 해당 상품 이미지를 볼 수 있게

--- src/pages/qna/QnAListItem.jsx
+++ src/pages/qna/QnAListItem.jsx
@@ -1,52 +1,79 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosInstance from '@hooks/useAxiosInstance';
 
-export default function QnAListItem({ item, number }) {
-  // product_id 유무에 따른 Link path 분기 처리
-  const detailPath = item.product_id
-    ? `/qna/product/detail/${item._id}`
-    : `/qna/detail/${item._id}`;
+const QnAListItem = ({ item, number }) => {
+  const axios = useAxiosInstance();
 
-    // 답변 완료 여부 체크 - admin이 작성한 댓글이 있는지 확인
-  const hasAdminReply = item.replies?.some(reply => reply.user?.type === 'admin');
+  // 댓글 목록을 조회하는 쿼리
+  const { data: repliesData } = useQuery({
+    queryKey: ['replies', item._id],
+    queryFn: () => axios.get(`/posts/${item._id}/replies`),
+    select: (res) => res.data,
+    staleTime: 1000 * 60, // 1분
+    cacheTime: 1000 * 60 * 5, // 5분
+  });
+
+  // 관리자 답변 여부 확인
+  const hasAdminReply = repliesData?.item?.some(
+    (reply) => reply.user?.email === 'admin@market.com'
+  );
 
   return (
-    <tr className='border-b border-grey-10'>
+    <tr className='border-b border-grey-10 hover:bg-grey-5'>
       <td className='py-5 text-left pl-5'>{number}</td>
-
-      <td className='py-5 text-left pl-5'>
+      <td>
         <Link
-          to={detailPath}
-          className='hover:text-secondary-20 transition-colors'
+          to={
+            item.product_id
+              ? `/qna/product/detail/${item._id}`
+              : `/qna/detail/${item._id}`
+          }
+          className='flex items-center py-5 pl-5 gap-2'
         >
-          {item.title}
+          {item.product && item.product.image && (
+            <img
+              src={`https://11.fesp.shop${item.product.image.path}`}
+              alt={item.product.name}
+              className='w-20 h-20 object-cover'
+            />
+          )}
+          <div className='flex items-center gap-2'>
+            {item.title}
+            <span
+              className={`inline-block px-5 py-2 rounded-[20px] text-white text-sm ${
+                hasAdminReply ? 'bg-primary-40' : 'bg-grey-20'
+              }`}
+            >
+              {hasAdminReply ? '답변완료' : '답변대기'}
+            </span>
+          </div>
         </Link>
-        <span
-          className={`inline-block px-5 py-2 rounded-[20px] text-white text-sm ml-2.5 ${
-            hasAdminReply  ? 'bg-primary-40' : 'bg-grey-20'
-          }`}
-        >
-          {hasAdminReply ? '답변완료' : '답변대기'}
-        </span>
       </td>
-      <td className='py-5 text-right pr-2.5'>{item.user.name}</td>
-      <td className='py-5 text-right pr-5'>
-        <div className='text-2xl'>{item.createdAt}</div>
-      </td>
+      <td className='py-5 text-right pr-2.5'>{item.user?.name}</td>
+      <td className='py-5 text-right pr-5'>{item.createdAt.split(' ')[0]}</td>
     </tr>
   );
-}
+};
+
+export default QnAListItem;
 
 QnAListItem.propTypes = {
   item: PropTypes.shape({
-    _id: PropTypes.number.isRequired,
-    title: PropTypes.string,
-    product_id: PropTypes.number,
+    _id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    title: PropTypes.string.isRequired,
     user: PropTypes.shape({
       name: PropTypes.string.isRequired,
     }),
     createdAt: PropTypes.string.isRequired,
-    repliesCount: PropTypes.number.isRequired,
-  }),
+    product_id: PropTypes.number,
+    product: PropTypes.shape({
+      image: PropTypes.shape({
+        path: PropTypes.string.isRequired,
+      }),
+      name: PropTypes.string,
+    }),
+  }).isRequired,
   number: PropTypes.number.isRequired,
 };
