14539f2a008c46eaabfd512255cf8c640371d881
jiyoungnim <cloundyon31@gmail.com>
2025-01-14 16:20:18 +0900

    📝 카카오톡으로부터 받은 인가코드를 이용해 Auth.jsx(rediect_uri: 인가코드 받을 경로)에서 가입유저의 개인정보를 post요청으로 가져온 후, 그 중 access_token을 token이라는 키 값으로 로컬 스토리지에 저장

--- src/pages/user/Auth.jsx
+++ src/pages/user/Auth.jsx
@@ -1,5 +1,52 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Auth() {
-  return <>Auth Page</>;
+  const navigate = useNavigate();
+  const API_KEY = "7b635f7b3d4379252462f78787fc908b";
+  const REDIRECT_URI = "http://localhost:5173/users/login/kakao";
+
+  const bringToken = async () => {
+    const authToken = new URL(window.location.href).searchParams.get("code");
+    console.log(authToken);
+
+    const res = await axios.post(
+      "https://kauth.kakao.com/oauth/token",
+      {
+        grant_type: "authorization_code",
+        client_id: API_KEY,
+        redirect_uri: REDIRECT_URI,
+        code: authToken,
+      },
+      {
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      }
+    );
+
+    return res.data;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await bringToken();
+        console.log("data: ", data);
+
+        if (data) {
+          localStorage.setItem("token", JSON.stringify(data.access_token));
+          navigate("/");
+        }
+      } catch (err) {
+        console.error("Error fetching token: ", err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <>🔥Auth Page🔥</>;
 }
 
 export default Auth;
