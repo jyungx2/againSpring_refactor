965fa56c0827371a9d4f1c2a03dcf751f7b16653
ohDASEUL <comeseul312@gmail.com>
2025-01-15 16:48:48 +0900

    💄[공지사항_목록] 목록 이미지 디자인 변경 및 등록된 이미지 없을 경우 대체 이미지 나오도록

--- src/pages/notice/NoticeListItem.jsx
+++ src/pages/notice/NoticeListItem.jsx
@@ -23,18 +23,35 @@ export default function NoticeListItem({ item }) {
     <div className='w-[280px]'>
       <div className='rounded w-[280px] h-[280px] bg-grey-10 overflow-hidden'>
         {firstImageUrl ? (
-          <img
-            src={firstImageUrl}
-            alt={item.title}
-            className='w-full h-full object-cover rounded'
-            onError={(e) => {
-              e.target.parentElement.classList.add('bg-grey-10');
-              e.target.style.display = 'none';
-            }}
-          />
+          <div className='relative w-full h-full'>
+            <img
+              src={firstImageUrl}
+              alt={item.title}
+              className='w-full h-full object-cover rounded blur-sm'
+              onError={(e) => {
+                e.target.parentElement.classList.add('bg-grey-10');
+                e.target.style.display = 'none';
+              }}
+            />
+            <div className='absolute inset-0 flex flex-col items-center justify-center gap-4'>
+              <div className='w-40 h-0.5 bg-white'></div>
+              <h1 className='text-4xl font-bold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
+                {item.title}
+              </h1>
+              <div className='w-40 h-0.5 bg-white'></div>
+            </div>
+          </div>
         ) : (
-          // 이미지가 없는 경우 기본 배경색 표시
-          <div className='w-full h-full bg-grey-10 rounded' />
+          <div className='relative'>
+            <img src='./images/notice-list-img.png' className='blur-sm' />
+            <div className='absolute inset-0 flex flex-col items-center justify-center gap-4'>
+              <div className='w-40 h-0.5 bg-white'></div>
+              <h1 className='text-4xl font-bold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
+                {item.title}
+              </h1>
+              <div className='w-40 h-0.5 bg-white'></div>
+            </div>
+          </div>
         )}
       </div>
       <div className='pt-[19px] pl-[10px] w-full'>
