c4c1749f77b28c2b1bcba1cb09393fbdb58e4d2f
jiyoungnim <cloundyon31@gmail.com>
2025-01-15 14:12:34 +0900

    📝 오픈마켓 API 서버를 직접 눈으로 확인하고 isNew 속성의 동작원리 이해 및 Auth.jsx 코드 작성 완료

--- src/pages/user/Auth.jsx
+++ src/pages/user/Auth.jsx
@@ -1,29 +1,54 @@
 import { useEffect } from "react";
 import useAxiosInstance from "@hooks/useAxiosInstance";
+import useUserStore from "@store/userStore";
+import { useNavigate } from "react-router-dom";
 
 function Auth() {
+  const navigate = useNavigate();
   const axios = useAxiosInstance();
-  const REDIRECT_URI = "http://localhost:5173/users/login/kakao";
+  const { setUser } = useUserStore();
+
   const authCode = new URL(window.location.href).searchParams.get("code");
+  const autoLogin = new URL(window.location.href).searchParams.get("state");
+
+  const REDIRECT_URI = "http://localhost:5173/users/login/kakao";
 
   const kakaoUserData = {
     code: authCode,
     redirect_uri: REDIRECT_URI,
     user: {},
   };
-  console.log(kakaoUserData);
+  // console.log(kakaoUserData);
+  const getKakaoUser = async (loginData) => {
+    try {
+      const res = await axios.post(`/users/login/kakao`, loginData);
+      const kakaoUser = res.data.item;
+      console.log("kakao user:", kakaoUser);
+
+      if (kakaoUser) {
+        setUser({
+          _id: kakaoUser._id,
+          name: kakaoUser.name,
+          type: kakaoUser.type,
+          profile: kakaoUser.image,
+          accessToken: kakaoUser.token.accessToken,
+          refreshToken: kakaoUser.token.refreshToken,
+          autoLogin,
+        });
+      }
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
-    const getKakaoUser = async (loginData) => {
-      try {
-        const res = await axios.post(`/users/login/kakao`, loginData);
-        console.log("res: ", res);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getKakaoUser(kakaoUserData);
-  }, [kakaoUserData]);
+    if (authCode) {
+      getKakaoUser(kakaoUserData);
+    } else {
+      console.error("Auth code is missing. Please check the URL parameters.");
+    }
+  }, [authCode]);
 
   return <>🔥Auth Page🔥</>;
 }
