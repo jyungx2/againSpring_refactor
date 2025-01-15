8fcf7e0f45efcdc88e506fb31c5113e36239da79
jiyoungnim <cloundyon31@gmail.com>
2025-01-15 14:51:18 +0900

    💡 userStore.js 주석 추가

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -22,23 +22,22 @@ const UserStore = (set) => ({
     set({ user: null });
     localStorage.removeItem("user");
     sessionStorage.removeItem("user");
-
-    if (localStorage.getItem("kakaoToken")) {
-      localStorage.removeItem("kakaoToken");
-    }
   },
 });
 
 // ✅ 앱을 처음 열 때(= 브라우저를 열었을 때) 실행되는 코드로, 이전에 저장된 사용자 정보를 불러온다. (👉 앱이 시작될 때 호출되어 초기 상태를 설정)
 // useUserStore는 스토어를 초기화하는 역할을 하며, persist 미들웨어를 사용하여 localStorage나 sessionStorage에서 사용자 정보를 읽어오는데, 사용자 정보 내에 있는 autoLogin값에 따라 그 값을 다시 local에 저장할지, session에 저장할지 결정된다.
+// 이때 session storage에 저장된 유저정보는 브라우저를 닫으며 바로 사라지기 때문에 사실상 세션스토리지에서 유저정보를 읽어오는 것 자체가 의미없지만, 우리 앱의 기본 동작은 세션스토리지에 저장하는 것이므로 이렇게 설정!
 const useUserStore = create(
   persist(UserStore, {
     name: "user",
+    // 앱이 처음 로드되었을 때(초기화 시) 저장소를 결정하기 위해 create() 내부에서도 storage 속성 설정
     storage: createJSONStorage(() => {
       // 위의 UserStore의 setUser 함수에서 autoLogin이라는 속성은 무조건 localStorage에 저장해줬었음. (이 값에 의해 자동로그인 여부가 결정되므로 항상 로컬에 킵하고 있어야 나중에 바로 로그인시킬지 말지 결정 가능하기 때문)
       const autoLogin = localStorage.getItem("autoLogin") === "true";
 
-      // autoLogin 값에 따라 저장소를 동적으로 선택
+      // 앱 초기화 시, 저장된 데이터를 적절한 저장소에서 읽어오기 위해 필요
+      // setUser는 사용자 행동에 의해 호출되지만, 앱 로드 시 초기화 과정은 setUser와 관계없이 항상 실행
       return autoLogin ? localStorage : sessionStorage;
     }),
   })
