2b0f859cbc0bbba8f26909d66cf95e32d331b69c
jiyoungnim <cloundyon31@gmail.com>
2025-01-15 13:10:44 +0900

    📝

--- src/pages/user/Auth.jsx
+++ src/pages/user/Auth.jsx
@@ -1,9 +1,29 @@
-import { useNavigate } from "react-router-dom";
-import useUserStore from "@store/userStore";
+import { useEffect } from "react";
+import useAxiosInstance from "@hooks/useAxiosInstance";
 
 function Auth() {
-  const navigate = useNavigate();
+  const axios = useAxiosInstance();
+  const REDIRECT_URI = "http://localhost:5173/users/login/kakao";
   const authCode = new URL(window.location.href).searchParams.get("code");
+
+  const kakaoUserData = {
+    code: authCode,
+    redirect_uri: REDIRECT_URI,
+    user: {},
+  };
+  console.log(kakaoUserData);
+
+  useEffect(() => {
+    const getKakaoUser = async (loginData) => {
+      try {
+        const res = await axios.post(`/users/login/kakao`, loginData);
+        console.log("res: ", res);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getKakaoUser(kakaoUserData);
+  }, [kakaoUserData]);
 
   return <>🔥Auth Page🔥</>;
 }
