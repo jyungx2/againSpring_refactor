f9878fba741e40d617d270ee1ec5fe93e0da9a8b
jiyoungnim <cloundyon31@gmail.com>
2025-01-16 14:38:15 +0900

    💄 OrderItem 스타일링 - 전체적인 레이아웃 변경

--- src/pages/user/OrderItem.jsx
+++ src/pages/user/OrderItem.jsx
@@ -9,21 +9,20 @@ function OrderItem({ products }) {
     return (
       <div
         key={item._id}
-        className="mt-[30px] flex flex-col gap-[20px] text-[16px] border border-grey-10 rounded-[12px] p-[20px] "
+        className="mt-[30px] flex gap-[20px] text-[16px] border border-grey-10 rounded-[12px] "
       >
-        <div className="flex justify-between">
-          <div className="text-[24px] font-gowunBold">
-            주문완료
-            <span className="text-[18px] text-secondary-50 font-gowunBold">
-              {" "}
-              · 12/30 (목) 도착
-            </span>
+        <div className="flex flex-col flex-grow justify-center p-[24px]">
+          <div className="flex justify-between">
+            <div className="text-[24px] font-gowunBold">
+              주문완료
+              <span className="text-[18px] text-secondary-50 font-gowunBold">
+                {" "}
+                · 12/30 (목) 도착
+              </span>
+            </div>
+            <img src={"/icons/dots.svg"} />
           </div>
-          <img src={"/icons/dots.svg"} />
-        </div>
-
-        <div className="flex items-center border border-primary-50 rounded-lg">
-          <div className="flex flex-grow gap-[20px]">
+          <div className="flex gap-[20px]">
             <img
               className="w-[150px] h-[200px] object-contain"
               src={`https://11.fesp.shop${item.image?.path}`}
@@ -35,26 +34,27 @@ function OrderItem({ products }) {
               </p>
             </div>
           </div>
-          <div className="flex flex-col gap-[20px] w-[240px] p-[20px] border-l border-grey-30">
-            <button
-              type="button"
-              className="text-[18px] border border-grey-20 h-[48px]"
-            >
-              배송 조회
-            </button>
-            <button
-              type="button"
-              className="text-[18px] border border-grey-20 h-[48px]"
-            >
-              교환, 반품 신청
-            </button>
-            <button
-              type="button"
-              className="text-[18px] text-primary-60 border border-primary-60 h-[48px]"
-            >
-              리뷰 작성하기
-            </button>
-          </div>
+        </div>
+
+        <div className="flex flex-col justify-center gap-[20px] w-[240px] p-[24px] border-l border-grey-20">
+          <button
+            type="button"
+            className="text-[18px] border border-grey-20 h-[48px] rounded-lg"
+          >
+            배송 조회
+          </button>
+          <button
+            type="button"
+            className="text-[18px] border border-grey-20 h-[48px] rounded-lg"
+          >
+            교환, 반품 신청
+          </button>
+          <button
+            type="button"
+            className="text-[18px] text-primary-60 border border-primary-60 h-[48px] rounded-lg"
+          >
+            리뷰 작성하기
+          </button>
         </div>
       </div>
     );
