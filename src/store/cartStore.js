9b55f212632a8b21cb55fcb9b890e714810abc5b
khk99 <khk990815@gmail.com>
2025-01-16 14:51:58 +0900

    ♻️ 중복 코드 제거

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -2,9 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import useUserStore from "@store/userStore";
 
-const axiosInstance = () => {
-  const { user } = useUserStore.getState();
-
+const axiosInstance = (user) => {
   return axios.create({
     baseURL: "https://11.fesp.shop",
     timeout: 1000 * 15,
@@ -19,213 +17,200 @@ const axiosInstance = () => {
   });
 };
 
-export const cartStore = create((set, get) => {
-  const instance = axiosInstance();
+export const cartStore = create((set, get) => ({
+  cartItemsList: [],
+  shippingCost: 0,
+  totalOrderAmount: 0,
+  loading: false,
+  error: null,
+  selectedItems: [],
 
-  const computeTotalOrderAmount = () => {
+  // 총 주문 금액 계산
+  computeTotalOrderAmount: () => {
     const { cartItemsList, shippingCost } = get();
     const totalPrice = cartItemsList.reduce(
       (total, item) => total + item.price * item.quantity,
       0
     );
     return totalPrice + shippingCost;
-  };
+  },
 
-  return {
-    cartItemsList: [],
-    shippingCost: 0,
-    totalOrderAmount: 0,
-    loading: false,
-    error: null,
-    selectedItems: [],
+  // 장바구니 목록 표시
+  fetchCartItems: async () => {
+    set({ loading: true, error: null });
 
-    // 장바구니 목록 표시
-    fetchCartItems: async () => {
-      set({ loading: true, error: null });
+    const { user } = useUserStore.getState();
 
-      const { user } = useUserStore.getState();
+    if (!user || !user.accessToken) {
+      console.error("Access Token이 존재하지 않습니다.");
+      set({
+        loading: false,
+        error: "로그인이 필요합니다.",
+      });
+      return;
+    }
 
-      if (!user || !user.accessToken) {
-        console.error("Access Token이 존재하지 않습니다.");
-        set({
-          loading: false,
-          error: "로그인이 필요합니다.",
-        });
-        return;
+    try {
+      const instance = axiosInstance(user);
+      const response = await instance.get("/carts/");
+
+      const products = response.data.item.map((item) => ({
+        id: item.product._id,
+        name: item.product.name,
+        price: item.product.price,
+        quantity: item.quantity,
+        image: item.product.image.path,
+        _id: item._id,
+        user_id: item.user_id || user._id,
+      }));
+
+      set({
+        cartItemsList: products,
+        shippingCost: response.data.cost.shippingFees,
+        totalOrderAmount: get().computeTotalOrderAmount(),
+        loading: false,
+      });
+    } catch (error) {
+      console.error(error.response?.data || error.message);
+      set({
+        loading: false,
+        error: "장바구니 아이템을 가져오는 데 실패했습니다.",
+      });
+    }
+  },
+
+  // 장바구니에 아이템 추가
+  addToCart: async (productId, quantity) => {
+    const { user } = useUserStore.getState();
+    const instance = axiosInstance(user);
+
+    try {
+      const requestBody = {
+        product_id: productId,
+        quantity: quantity,
+      };
+
+      const response = await instance.post("/carts/", requestBody);
+
+      if (response.status === 201) {
+        await get().fetchCartItems();
+        set({ totalOrderAmount: get().computeTotalOrderAmount() });
       }
+    } catch (error) {
+      console.error(error);
+      set({ error: "장바구니에 아이템 추가 실패." });
+    }
+  },
+
+  // 수량 변경
+  updateItemQuantity: async (productId, newQuantity) => {
+    const { cartItemsList } = get();
+    const cartItem = cartItemsList.find((item) => item.id === productId);
+
+    if (cartItem) {
+      const { user } = useUserStore.getState();
+      const instance = axiosInstance(user);
 
       try {
-        const response = await instance.get("/carts/", {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
+        const response = await instance.patch(`/carts/${cartItem._id}`, {
+          quantity: newQuantity,
         });
 
-        const products = response.data.item.map((item) => ({
-          id: item.product._id,
-          name: item.product.name,
-          price: item.product.price,
-          quantity: item.quantity,
-          image: item.product.image.path,
-          _id: item._id,
-          user_id: item.user_id || user._id,
-        }));
+        if (response.status === 200) {
+          const updatedCartItemsList = cartItemsList.map((item) =>
+            item.id === productId ? { ...item, quantity: newQuantity } : item
+          );
 
-        set({
-          cartItemsList: products,
-          shippingCost: response.data.cost.shippingFees,
-          totalOrderAmount: computeTotalOrderAmount(),
-          loading: false,
-        });
+          set({
+            cartItemsList: updatedCartItemsList,
+            totalOrderAmount: get().computeTotalOrderAmount(),
+          });
+        } else {
+          set({ error: "장바구니 상품 수량 변경 실패." });
+        }
       } catch (error) {
         console.error(error.response?.data || error.message);
         set({
-          loading: false,
-          error: "장바구니 아이템을 가져오는 데 실패했습니다.",
+          error: "장바구니 상품 수량 변경 실패.",
         });
       }
-    },
+    } else {
+      console.error("해당 상품 ID에 대한 장바구니 상품이 없음", productId);
+    }
+  },
 
-    // 장바구니에 아이템 추가
-    addToCart: async (productId, quantity) => {
-      const { user } = useUserStore.getState();
-      const instance = axiosInstance();
+  // 체크박스 선택한 상품 추가
+  selectItem: (id) => {
+    set((state) => ({
+      selectedItems: [...state.selectedItems, id],
+    }));
+  },
 
-      try {
-        // 요청 본문 준비
-        const requestBody = {
-          product_id: productId,
-          quantity: quantity,
-        };
+  // 체크박스 선택 해제
+  deselectItem: (id) => {
+    set((state) => ({
+      selectedItems: state.selectedItems.filter((itemId) => itemId !== id),
+    }));
+  },
 
-        const response = await instance.post("/carts/", requestBody, {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
-        });
+  // 선택한 상품 삭제
+  deleteSelectedItems: async () => {
+    const { selectedItems, cartItemsList } = get();
+    const { user } = useUserStore.getState();
 
-        if (response.status === 201) {
-          await get().fetchCartItems();
-        }
-      } catch (error) {
-        console.error(error);
-        set({ error: "장바구니에 아이템 추가 실패." });
-      }
-    },
+    if (selectedItems.length === 0) {
+      set({ error: "선택한 상품이 없습니다." });
+      return;
+    }
 
-    // 수량 변경
-    updateItemQuantity: async (productId, newQuantity) => {
-      const { cartItemsList } = get();
-      const cartItem = cartItemsList.find((item) => item.id === productId);
+    const selectedCartItemIds = cartItemsList
+      .filter((item) => selectedItems.includes(item.id))
+      .map((item) => item._id);
 
-      if (cartItem) {
-        const { user } = useUserStore.getState();
+    try {
+      const instance = axiosInstance(user);
+      await instance.delete(`/carts/`, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+        data: { carts: selectedCartItemIds },
+      });
 
-        try {
-          const response = await instance.patch(
-            `/carts/${cartItem._id}`,
-            { quantity: newQuantity },
-            {
-              headers: {
-                Authorization: `Bearer ${user.accessToken}`,
-              },
-            }
-          );
-
-          if (response.status === 200) {
-            const updatedCartItemsList = cartItemsList.map((item) =>
-              item.id === productId ? { ...item, quantity: newQuantity } : item
-            );
-
-            set({
-              cartItemsList: updatedCartItemsList,
-              totalOrderAmount: computeTotalOrderAmount(),
-            });
-          } else {
-            set({ error: "장바구니 상품 수량 변경 실패." });
-          }
-        } catch (error) {
-          console.error(error.response?.data || error.message);
-          set({
-            error: "장바구니 상품 수량 변경 실패.",
-          });
-        }
-      } else {
-        console.error("해당 상품 ID에 대한 장바구니 상품이 없음", productId);
-      }
-    },
-
-    // 체크박스 선택한 상품 추가
-    selectItem: (id) => {
       set((state) => ({
-        selectedItems: [...state.selectedItems, id],
+        cartItemsList: state.cartItemsList.filter(
+          (item) => !selectedItems.includes(item.id)
+        ),
+        selectedItems: [],
+        error: null,
       }));
-    },
 
-    // 체크박스 선택 해제
-    deselectItem: (id) => {
-      set((state) => ({
-        selectedItems: state.selectedItems.filter((itemId) => itemId !== id),
-      }));
-    },
+      alert("선택한 상품이 삭제되었습니다.");
+    } catch {
+      set({ error: "상품 삭제에 실패했습니다." });
+    }
+  },
 
-    // 선택한 상품 삭제
-    deleteSelectedItems: async () => {
-      const { selectedItems, cartItemsList } = get();
-      const { user } = useUserStore.getState();
+  // 장바구니 비우기
+  clearCart: async () => {
+    const { user } = useUserStore.getState();
 
-      if (selectedItems.length === 0) {
-        set({ error: "선택한 상품이 없습니다." });
-        return;
-      }
+    try {
+      const instance = axiosInstance(user);
+      await instance.delete("/carts/cleanup", {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      });
 
-      const selectedCartItemIds = cartItemsList
-        .filter((item) => selectedItems.includes(item.id))
-        .map((item) => item._id);
-
-      try {
-        await instance.delete(`/carts/`, {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
-          data: { carts: selectedCartItemIds },
-        });
-
-        set((state) => ({
-          cartItemsList: state.cartItemsList.filter(
-            (item) => !selectedItems.includes(item.id)
-          ),
-          selectedItems: [],
-          error: null,
-        }));
-
-        alert("선택한 상품이 삭제되었습니다.");
-      } catch {
-        set({ error: "상품 삭제에 실패했습니다." });
-      }
-    },
-
-    // 장바구니 비우기
-    clearCart: async () => {
-      const { user } = useUserStore.getState();
-
-      try {
-        await instance.delete("/carts/cleanup", {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
-        });
-
-        set({
-          cartItemsList: [],
-          selectedItems: [],
-          error: null,
-        });
-      } catch {
-        set({ error: "장바구니 비우기에 실패했습니다." });
-      }
-    },
-  };
-});
+      set({
+        cartItemsList: [],
+        selectedItems: [],
+        error: null,
+      });
+    } catch {
+      set({ error: "장바구니 비우기에 실패했습니다." });
+    }
+  },
+}));
 
 export default cartStore;
--- src/store/wishlistStore.js
+++ src/store/wishlistStore.js
@@ -2,9 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import useUserStore from "@store/userStore";
 
-const axiosInstance = () => {
-  const { user } = useUserStore.getState();
-
+const axiosInstance = (user) => {
   return axios.create({
     baseURL: "https://11.fesp.shop",
     timeout: 1000 * 15,
@@ -19,97 +17,87 @@ const axiosInstance = () => {
   });
 };
 
-export const wishlistStore = create((set) => {
-  const instance = axiosInstance();
+export const wishlistStore = create((set) => ({
+  wishlistItems: [],
+  loading: false,
+  error: null,
 
-  return {
-    wishlistItems: [],
-    loading: false,
-    error: null,
+  // 위시리스트 목록 조회
+  fetchWishlistItems: async () => {
+    set({ loading: true, error: null });
 
-    // 위시리스트 목록 조회
-    fetchWishlistItems: async () => {
-      set({ loading: true, error: null });
+    const { user } = useUserStore.getState();
 
-      const { user } = useUserStore.getState();
+    if (!user || !user.accessToken) {
+      console.error("Access Token이 존재하지 않습니다.");
+      set({
+        loading: false,
+        error: "로그인이 필요합니다.",
+      });
+      return;
+    }
 
-      if (!user || !user.accessToken) {
-        console.error("Access Token이 존재하지 않습니다.");
+    try {
+      const instance = axiosInstance(user);
+      const response = await instance.get("/bookmarks/product");
+
+      if (!response.data.item || response.data.item.length === 0) {
         set({
+          wishlistItems: [],
           loading: false,
-          error: "로그인이 필요합니다.",
         });
         return;
       }
 
-      try {
-        const response = await instance.get("/bookmarks/product", {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
-        });
+      const wishlist = response.data.item.map((item) => ({
+        id: item.product._id,
+        name: item.product.name,
+        price: item.product.price,
+        image: item.product.mainImages[0]?.path
+          ? `https://11.fesp.shop${item.product.mainImages[0].path}`
+          : "/path/to/default/image.png",
+        _id: item._id,
+      }));
 
-        if (!response.data.item || response.data.item.length === 0) {
-          set({
-            wishlistItems: [],
-            loading: false,
-          });
-          return;
-        }
+      set({
+        wishlistItems: wishlist,
+        loading: false,
+      });
+    } catch (error) {
+      console.error(
+        "Error fetching wishlist items",
+        error.response?.data || error.message
+      );
+      set({
+        loading: false,
+        error: "위시리스트를 가져오는 데 실패했습니다.",
+      });
+    }
+  },
 
-        const wishlist = response.data.item.map((item) => ({
-          id: item.product._id,
-          name: item.product.name,
-          price: item.product.price,
-          image: item.product.mainImages[0]?.path
-            ? `https://11.fesp.shop${item.product.mainImages[0].path}`
-            : "/path/to/default/image.png",
-          _id: item._id,
-        }));
+  // 위시리스트 삭제
+  deleteItem: async (itemId) => {
+    const { user } = useUserStore.getState();
 
-        set({
-          wishlistItems: wishlist,
-          loading: false,
-        });
-      } catch (error) {
-        console.error(
-          "Error fetching wishlist items",
-          error.response?.data || error.message
-        );
-        set({
-          loading: false,
-          error: "위시리스트를 가져오는 데 실패했습니다.",
-        });
-      }
-    },
+    try {
+      const instance = axiosInstance(user);
+      await instance.delete(`/bookmarks/${itemId}`);
 
-    // 위시리스트 삭제
-    deleteItem: async (itemId) => {
-      const { user } = useUserStore.getState();
-
-      try {
-        await instance.delete(`/bookmarks/${itemId}`, {
-          headers: {
-            Authorization: `Bearer ${user.accessToken}`,
-          },
-        });
-
-        set((state) => ({
-          wishlistItems: state.wishlistItems.filter(
-            (item) => item._id !== itemId
-          ),
-        }));
-      } catch (error) {
-        console.error(
-          "Error deleting wishlist item",
-          error.response?.data || error.message
-        );
-        set({
-          error: "위시리스트 삭제에 실패했습니다.",
-        });
-      }
-    },
-  };
-});
+      set((state) => ({
+        wishlistItems: state.wishlistItems.filter(
+          (item) => item._id !== itemId
+        ),
+      }));
+    } catch (error) {
+      console.error(
+        "Error deleting wishlist item",
+        error.response?.data || error.message
+      );
+      set({
+        error: "위시리스트 삭제에 실패했습니다.",
+      });
+    }
+  },
+}));
 
 export default wishlistStore;
