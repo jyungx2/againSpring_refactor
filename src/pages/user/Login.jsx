bbc820fc6a416cc88fe1f0dad203aa669f6a7e1b
jiyoungnim <cloundyon31@gmail.com>
2025-01-17 09:14:42 +0900

    📝

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
     setError,
     formState: { errors },
   } = useForm({
-    defaultValues: { email: "kimejoa@market.com", password: "11111111" },
+    defaultValues: { email: "u1@market.com", password: "11111111" },
   });
 
   const login = useMutation({
