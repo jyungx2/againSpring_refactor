777b773252a8fdfeb7ea57ca1df35a9577d0a3d1
jiyoungnim <cloundyon31@gmail.com>
2025-01-18 14:22:33 +0900

    💄 주문 상세조회 페이지 스타일링 완료

--- src/pages/user/OrderDetail.jsx
+++ src/pages/user/OrderDetail.jsx
@@ -1,7 +1,8 @@
 import Sidebar from "@pages/user/Sidebar";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function OrderDetail() {
+  const navigate = useNavigate();
   const location = useLocation();
   const bundle = location.state.bundle;
   const products = location.state.products;
@@ -10,7 +11,7 @@ function OrderDetail() {
     return (
       <div
         key={item._id}
-        className="mt-[30px] flex text-[16px] border border-grey-20 rounded-[12px] "
+        className="mt-[30px] mb-[20px] flex text-[16px] border border-grey-20 rounded-[12px] "
       >
         <div className="flex flex-col flex-grow justify-center p-[20px]">
           <div className="flex justify-between">
@@ -37,7 +38,7 @@ function OrderDetail() {
           </div>
         </div>
 
-        <div className="flex flex-col justify-center gap-[12px] w-[240px] px-[30px] border-l border-grey-20">
+        <div className="flex flex-col justify-center gap-[12px] w-[240px] px-[30px] border-l border-grey-20 font-gowunBold">
           <button
             type="button"
             className="text-[16px] border border-grey-20 h-[46px] rounded-lg"
@@ -68,10 +69,10 @@ function OrderDetail() {
         <Sidebar />
 
         <div className="flex-grow min-w-0 basis-0 flex flex-col gap-[40px]">
-          <div id="1" className="flex flex-col gap-1">
+          <div id="1" className="flex flex-col pt-10">
             <div>
-              <h1 className="mb-4 font-gowunBold text-[20px]">주문상세</h1>
-              <p className="text-[14px]">
+              <h1 className="mb-4 font-gowunBold text-[22px]">주문상세</h1>
+              <p className="text-[16px]">
                 <span className="font-gowunBold">2025.1.14 주문</span> 주문번호
                 xxxxxxxx
               </p>
@@ -79,55 +80,66 @@ function OrderDetail() {
             <div>{renderedItem}</div>
           </div>
 
-          <div id="2">
-            <h2 className="mb-6 font-gowunBold">받는 사람 정보</h2>
+          <div className="flex flex-col">
+            <h2 className="mb-6 font-gowunBold text-[18px]">받는 사람 정보</h2>
             <div className="flex flex-col border-t-2 border-black ">
               <table className="w-full border-collapse my-[20px]">
-                <thead className="bg-grey-5 font-gowunBold">
-                  <tr>
-                    <th className="border border-grey-30 text-center p-[8px]">
-                      번호
-                    </th>
-                    <th className="border border-grey-30 text-center p-[8px]">
-                      제목
-                    </th>
-                    <th className="border border-grey-30 text-center p-[8px]">
-                      작성일
-                    </th>
-                    <th className="border border-grey-30 text-center p-[8px]">
-                      작성자
-                    </th>
-                    <th className="border border-grey-30 text-center p-[8px]">
-                      조회수
-                    </th>
-                  </tr>
-                </thead>
-
                 <tbody>
-                  <tr className="hover:bg-primary-5 hover:cursor-pointer">
-                    <td className="border border-grey-30 text-center p-[8px]">
-                      1
+                  <tr>
+                    <td className="p-[8px] w-[12.5%]">받는사람</td>
+                    <td className="p-[8px]">이지영</td>
+                  </tr>
+                  <tr>
+                    <td className="p-[8px] w-[12.5%]">연락처</td>
+                    <td className="p-[8px]">010-xxxx-xxxx</td>
+                  </tr>
+                  <tr>
+                    <td className="p-[8px] w-[12.5%]">받는주소</td>
+                    <td className="p-[8px]">
+                      (25102) 멋사광역시 프론트구 개발자동 xx아파트 xx동 xx호
                     </td>
-                    <td className="border border-grey-30 text-center p-[8px]">
-                      배송은 보통 며칠 걸리나요?
-                    </td>
-                    <td className="border border-grey-30 text-center p-[8px]">
-                      2024-12-02
-                    </td>
-                    <td className="border border-grey-30 text-center p-[8px]">
-                      국연수
-                    </td>
-                    <td className="border border-grey-30 text-center p-[8px]">
-                      2
-                    </td>
+                  </tr>
+                  <tr>
+                    <td className="p-[8px] w-[12.5%]">배송요청사항</td>
+                    <td className="p-[8px]">문 앞</td>
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
 
-          <div id="3">
-            <h2>결제 정보</h2>
+          <div className="flex flex-col">
+            <h2 className="mb-6 font-gowunBold text-[18px]">결제정보</h2>
+            <div className="flex flex-col gap-6 border-t-2 border-black pt-6">
+              <div className="flex flex-col gap-2 border-b border-grey-30 pb-6">
+                <span>결제수단</span>
+                <p className="text-[16px] font-gowunBold">
+                  MS 멋사카드 / 3 개월
+                </p>
+              </div>
+              <div className="flex flex-col border-b border-grey-30 pb-6">
+                <table>
+                  <tbody>
+                    <tr className="flex justify-between">
+                      <td className="p-[8px] w-[12.5%]">총 상품가격</td>
+                      <td className="p-[8px] font-gowunBold">xxxx원</td>
+                    </tr>
+                    <tr className="flex justify-between">
+                      <td className="p-[8px] w-[12.5%]">배송비</td>
+                      <td className="p-[8px] font-gowunBold">0</td>
+                    </tr>
+                    <tr className="flex justify-between">
+                      <td className="p-[8px] w-[12.5%] text-[20px]">
+                        총 결제금액
+                      </td>
+                      <td className="p-[8px] font-gowunBold text-[20px]">
+                        xxxx원
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
