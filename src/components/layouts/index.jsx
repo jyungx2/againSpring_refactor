0343317853df96bd43901e45c59f796f2f5600ab
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-01-20 09:09:52 +0900

    tansointro 페이지 헤더와 푸터 숨기기

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -1,21 +1,26 @@
 import Header from "@components/layouts/Header";
 import Footer from "@components/layouts/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import PropTypes from "prop-types";
 
 const MainLayout = ({ title, description }) => {
+  const location = useLocation();
+
+  // 헤더와 푸터 숨기기
+  const hideHeaderFooter = location.pathname === "/tansointro";
+
   return (
     <div className="flex flex-col min-h-screen">
       <Helmet>
         <title>{title || "다시,봄 메인 페이지"}</title>
         <meta name="description" content={description || "다시,봄 홈페이지"} />
       </Helmet>
-      <Header />
-      <main className="flex-grow container mx-auto px-4 py-8">
+      {!hideHeaderFooter && <Header />} {/*tansointro 헤더 숨기기 */}
+      <main className={`flex-grow ${hideHeaderFooter ? "" : "container mx-auto px-4 py-8"}`}>
         <Outlet />
       </main>
-      <Footer />
+      {!hideHeaderFooter && <Footer />} {/*푸터 숨기기 */}
     </div>
   );
 };
