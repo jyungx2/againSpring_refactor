80f7ec7714a1911ceeff82a1d83ea0bfb7bfcec7
ohDASEUL <comeseul312@gmail.com>
2025-01-21 20:27:28 +0900

    🔧 vite.config.js 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -17,22 +17,11 @@ export default defineConfig({
   },
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true,
-      include: [/node_modules/],
+      include: [/node_modules/], // 번들에 포함시킬 모듈의 경로
+      extensions: [".js", ".cjs"], // CommonJS 모듈로 간주할 파일의 확장자
+      strictRequires: true, // require 구문에 해당 모듈이 없을 경우 에러 발생
+      transformMixedEsModules: true, // import와 require문을 함께 사용하는 경우 이를 번들에 포함시키기 위함
     },
-    rollupOptions: {
-      output: {
-        format: "es",
-      },
-    },
-  },
-  optimizeDeps: {
-    include: [
-      "quill",
-      "react-quilljs",
-      "sweetalert2",
-      "sweetalert2-react-content",
-    ],
   },
   css: {
     modules: {
