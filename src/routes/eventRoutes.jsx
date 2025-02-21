2a2027a2af499987ecd1ce0c487059aec4f4b485
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-21 17:41:20 +0900

    ✨ 이벤트 라우트에 유지보수 로더 및 에러 페이지 추가

--- src/routes/eventRoutes.jsx
+++ src/routes/eventRoutes.jsx
@@ -1,9 +1,22 @@
 import EventMainPage from "@pages/eventPage/EventMainPage";
 import EventDetailPage from "@pages/eventPage/EventDetailPage";
+import MaintenancePage from "@pages/MaintenancePage";
+import { maintenanceLoader } from "../utils/maintenanceLoader";
 
 const eventRoutes = [
-  { path: "event", element: <EventMainPage /> },
-  { path: "event/detail/:id", element: <EventDetailPage /> },
+  {
+    path: "event",
+    element: <EventMainPage />,
+    loader: maintenanceLoader,
+    errorElement: <MaintenancePage />
+  },
+  {
+    path: "event/detail/:id",
+    element: <EventDetailPage />,
+    loader: maintenanceLoader,
+    errorElement: <MaintenancePage />
+  },
 ];
+
 
 export default eventRoutes;
